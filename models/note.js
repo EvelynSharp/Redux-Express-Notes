e3e13541681180b413099d7789b049f09a3476be
Evelyn Sharp <yihongshi90@gmail.com>
2017-05-30 20:00:56 -0600

    initial commit

---
+++ models/note.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Note = new Schema({
+  title: { type: String, required: true },
+  body: String,
+  createdAt: { type: Date, default: Date.now() },
+  updatedAt: { type: Date, default: Date.now() }
+});
+
+module.exports = mongoose.model('Note', Note);
